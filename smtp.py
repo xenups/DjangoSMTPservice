614acf30eddd34af258de3d34b52e268059ef478
xenups <xenups@outlook.com>
2022-03-06 16:55:42 +0330

    code has been refactored "arguments tuples converted to the class"

--- smtp.py
+++ smtp.py
@@ -9,34 +9,39 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
 
+class MailConfig:
+    def __init__(self, user_name, password, provider, from_email=None, reply_email=None, config=None):
+        self.username = user_name
+        self.password = password
+        self.provider = provider
+        self.from_email = from_email or settings.DEFAULT_FROM_EMAIL
+        self.reply_email = reply_email or None
+        self.config = config or settings.DEFAULT_EMAIL_CONFIG
+
+
 class SMTPService(object):
     MAX_RETRY = 2
     AUTHENTICATION_FAILED_CODE = 454
     required_config_fields = ['username', 'password', 'host', 'port']
 
-    def __init__(self, from_email=settings.DEFAULT_FROM_EMAIL, reply_email=None, config=settings.DEFAULT_EMAIL_CONFIG):
+    def __init__(self, mail_config: MailConfig):
+        self.mail_config: MailConfig = mail_config
+        self._set_tls(self.mail_config.config.get('tls'))
+        self._smtp_service = smtplib.SMTP(self.mail_config.config['host'], self.mail_config.config['port'])
+        self._smtp_service.login(self.mail_config.username, self.mail_config.password)
 
-        self._provider = config.get('provider')
-        self._from_email = from_email
-        self._reply_email = reply_email
-
-        self._username = config['username']
-        self._password = config['password']
-
-        self._set_tls(config.get('tls'))
-        self._smtp_service = smtplib.SMTP(config['host'], config['port'])
-        self._smtp_service.login(self._username, self._password)
-
-    def send_mail(self, subject: str, receiver: [], html_msg, filepath=None, filename=None, quite=True):
+    def send_mail(self, subject: str, receiver: list, html_msg: str, filepath=None, filename=None, quite=True):
+        mails = []
         for _, address in enumerate(receiver):
             mail = self._create_mail_body(subject, address, html_msg)
             mail = self._add_mail_attachment(filepath, filename, mail)
-            self._process_mail(mail, quite)
+            mails.append(mail)
+        self._process_mail(mails, quite)
 
     def _create_mail_body(self, subject: str, address: str, html_msg) -> MIMEMultipart:
         message = MIMEMultipart()
         message['Subject'] = email.header.Header(force_text(subject), 'utf-8')
-        message['From'] = self._from_email
+        message['From'] = self.mail_config.from_email
         message['To'] = address
         message = self._add_aws_headers(message)
         body = MIMEText(html_msg.encode('utf-8'), 'html', 'utf-8')
@@ -60,7 +65,7 @@ class SMTPService(object):
                 raise TypeError("File doesn't exist, and no data given.")
         return message
 
-    def _process_mail(self, mails: [], quite: bool):
+    def _process_mail(self, mails: list, quite: bool):
         sent_mail_count: int = 0
         current_retry: int = 0
         while sent_mail_count < len(mails):
@@ -84,7 +89,7 @@ class SMTPService(object):
             self._smtp_service.starttls()
 
     def _add_aws_headers(self, message):
-        if self._provider == 'AWS':
+        if self.mail_config.provider == 'AWS':
             if settings.SES_CONFIGURATION_SET:
                 message['X-SES-CONFIGURATION-SET'] = settings.SES_CONFIGURATION_SET
         return message
